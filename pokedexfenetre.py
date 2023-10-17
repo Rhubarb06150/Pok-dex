c5991eeb6a8d46f57857769fecd6e6570814921f
Rhubarb06150 <116634580+Rhubarb06150@users.noreply.github.com>
2023-10-17 20:38:34 +0200

    Add files via upload

--- pokedexfenetre.py
+++ pokedexfenetre.py
@@ -3,36 +3,44 @@ import tkinter as tk
 from tkinter import *
 import random
 import keyboard
-import winsound
 
 #Fonctions bouttons
 
 def choixgeneration():
-    fenetrechoixgeneration=Tk()
+    gen.pack_forget()
     root.title('Choix de la génération')
-    width = 240
-    height = 300
-    fenetrechoixgeneration.geometry('300x240+50+50')
-    fenetrechoixgeneration.attributes('-alpha', 1)
-    fenetrechoixgeneration.resizable(False,False)
-    fenetrechoixgeneration.iconbitmap('images/icones/hooh.ico')
-    filename = 'sons/clicmenu.wav'
-    winsound.PlaySound(filename, winsound.SND_FILENAME)
+    gen1.pack()
+    gen2.pack()
 
+def choixgen1():
+    gen1.pack_forget()
+    gen2.pack_forget()
+    root.title('Pokédex (Première Génération)')
+    root.iconbitmap('images/icones/dracaufeu.ico')
+    messagebox.showinfo(title="Choix de génération", message='Vous avez choisi la première génération, soit les jeux: Pokémon Rouge, Pokémon Bleu et Pokémon Jaune')
+    
+def choixgen2():
+    gen1.pack_forget()
+    gen2.pack_forget()
+    root.title('Pokédex (Deuxième Génération)')
+    root.iconbitmap('images/icones/hooh.ico')
+    messagebox.showinfo(title="Choix de génération",message='Vous avez choisi la deuxième génération, soit les jeux: Pokémon Or, Pokémon Argent et Pokémon Cristal')
+    
 fond=random.randint(1,5)
 cheminfond="images/fonds/fond"+str(fond)+".png"
 root=Tk()
-root.title('Pokédex')
+root.title('Pokédex (Première Génération)')
 width = 680
 height = 576
 root.geometry('640x576+50+50')
 root.attributes('-alpha', 1)
 root.resizable(False,False)
-root.iconbitmap('images/icones/hooh.ico')
+root.iconbitmap('images/icones/dracaufeu.ico')
 bg = PhotoImage(file = cheminfond)
 label1 = Label( root, image = bg) 
 label1.place(x = 0, y = 0)
 gen=Button(root, text = "Génération",relief=RIDGE,bd=5,padx=50,command=choixgeneration)
-gen.pack()
-
+gen1=Button(root, text='Première génération',command=choixgen1)
+gen2=Button(root, text='Deuxième génération',command=choixgen2)
+gen.place(x=5,y=540)
 root.mainloop()
